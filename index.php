0e7e4e2a78c4870818c8de62f5cda19120d0ae36
cain <anniejocain@gmail.com>
2013-09-19 10:59:08 -0400

    turn autoload back on

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ $f3->set('library_codes', $library_codes);
 
 $f3->set('map_it_key',$map_it_key);
 
-//$f3->set('AUTOLOAD','api/; web/;');
+$f3->set('AUTOLOAD','api/; web/;');
 
 $f3->route('GET /api/locate', 'Locate->call_number');
 $f3->route('GET /api/locate/@location/@collection/@callno', 'Locate->call_number');
