7bcec80ff2a803853edd811d72add4ce41c7aba4
cain <anniejocain@gmail.com>
2013-10-09 09:46:26 -0400

    list of maps, finished row numbering closes #8

--- index.php
+++ index.php
@@ -121,6 +121,15 @@ $f3->route('GET /admin/check', function($f3) {
     echo $view->render('web/admin/check.html');
 });
 
+$f3->route('GET /admin/maps', function($f3) {
+    $f3->set('key',$f3->get('map_it_key'));
+    $f3->set('user',$f3->get('ADMIN_USER'));
+    $f3->set('password',$f3->get('ADMIN_PASSWORD'));
+    $f3->set('www',$f3->get('MAP_IT_HOME'));
+    $view=new View;
+    echo $view->render('web/admin/maps.html');
+});
+
 $f3->run();
 
 ?>
